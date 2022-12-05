81e9167af8818aae05775f0333314a279727eb35
Sayma Haque <sayma.haque@ucalgary.ca>
2022-12-04 22:42:28 -0700

    Create HomePage.php

---
+++ SPESH/Website/HomePage.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+<style> 
+body {
+  background-image: url("https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%2Fimages%3Fk%3Dpastel%2Bpink%2Bwhite%2Bbackgrounds&psig=AOvVaw0cYMKv7DKB2JAFUDCDF4gT&ust=1670289119178000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCOio1f6l4fsCFQAAAAAdAAAAABAE");
+  background-image: linear-gradient(#DBCCD4, #FFFFFF);
+}
+</style>
+</head>
+	<body>
+		<audio src="https://www.youtube.com/watch?v=OYvTbSU6TC0&ab_channel=WhenAllisSaid%26Done" loop autoplay ></audio>
+
+		<h1 style="color: #6D4F60; font-size:60px; text-align:center;">SPESH Market! </h1>
+		
+		
+
+		<p  style = "text-align:center; font-size:20px;"> Shop online at SPESH wherever, whenever to explore our diverse selection of international ingredients. It's never been easier to have the taste of home right at your finger tips :D</p>
+		
+		
+		<p style = "color: #6A475A;text-align:center; font-size:30px;"> <strong> EXPLORE SPESH NOW </strong></p>
+		
+		<a href="SearchCountry.html"><button style = "background-color:#F6F6F6; width:200px; height:60px">Countries </button></a>
+		<a href="SearchProduct.html"><button style = "background-color:#F6F6F6; width:200px; height:60px">Products </button></a>
+		<a href="SearchCategory.html"><button style = "background-color:#F6F6F6; width:200px; height:60px">Categories </button></a>
+		
+		<p title="Ur ugly"style="color: #4F3142; font-family: Comic Sans MS; text-align:center;">
+		I <3 U </p>
+		<a href="Login.html"><button style = "background-color:#F6F6F6; width:200px; height:60px">Sign in / Register </button></a>
+		
+		<p> </p>
+		<p> </p>
+		<p> </p>
+		<p> </p>
+		<p> </p>
+		<img style = align:center; src="https://www.pngkey.com/png/full/8-87499_image-download-design-watercolour-flowers-painting-hd-watercolor.png" style="width:10px">
+		<p> </p>
+		
+		
+		<!--
+		<p title="Ur ugly" style="color: #DBCCD4; font-size:50px;">Hover ur mouse here.</p>
+		
+		<img id="myImage" src="https://media.istockphoto.com/id/514572422/photo/baby-aspin.jpg?s=612x612&w=0&k=20&c=VhW1L24ptmR8lGz9BZuvPFX71NOIrGGSmBnRi5RjeF0=" style="width:500px">
+		<p></p>
+		
+		<button onclick="document.getElementById('myImage').src=
+			'https://media.istockphoto.com/id/514572422/photo/baby-aspin.jpg?s=612x612&w=0&k=20&c=VhW1L24ptmR8lGz9BZuvPFX71NOIrGGSmBnRi5RjeF0='">Happy Llama</button>
+		
+		<button onclick="document.getElementById('myImage').src=
+			'https://davisfarmland.com/wp-content/uploads/2020/06/sad-llama.jpg'">Sad llama</button>		
+		
+		<p></p>
+		<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Press Here</a>
+		<p></p>
+		
+		
+		<img src="http://www.clker.com/cliparts/2/7/5/9/15137959191911610634public-market-sketch.hi.png" style="width:300px">
+		<p> Donkey for my wife </p>
+		-->
+	</body>
+</html>
