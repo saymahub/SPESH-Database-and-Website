f2912ef9785d3b43b4aaaa48d9b3f2a13adfa0e2
Michele Pham <michele_pham@hotmail.ca>
2022-12-06 11:14:56 -0700

    Create ContactDistributor.php

---
+++ SPESH/Website/ContactDistributor.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <style> 
+        body {
+            height: 100%;
+            margin: 0;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-image: url("https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%2Fimages%3Fk%3Dpastel%2Bpink%2Bwhite%2Bbackgrounds&psig=AOvVaw0cYMKv7DKB2JAFUDCDF4gT&ust=1670289119178000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCOio1f6l4fsCFQAAAAAdAAAAABAE");
+            background-image: linear-gradient(#FFFFFF, #9E7E88);
+        }
+
+        .container { 
+        height: 200px;
+        position: relative;
+        }
+
+        .center {
+        margin: 0;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        -ms-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+        }
+
+    </style>
+</html>
+
