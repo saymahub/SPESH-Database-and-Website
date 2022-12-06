3d1393f30316b4fee6b7daecb37ce0f61e4911bd
Sayma Haque <sayma.haque@ucalgary.ca>
2022-12-06 03:54:14 -0700

    214

---
+++ SPESH/Website/ConfirmationPage.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style> 
+	body {
+		height: 100%;
+		margin: 0;
+		background-repeat: no-repeat;
+		background-attachment: fixed;
+		background-image: url("https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%2Fimages%3Fk%3Dpastel%2Bpink%2Bwhite%2Bbackgrounds&psig=AOvVaw0cYMKv7DKB2JAFUDCDF4gT&ust=1670289119178000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCOio1f6l4fsCFQAAAAAdAAAAABAE");
+		background-image: linear-gradient(#FFFFFF, #9E7E88);
+	}
+
+</style> 
+<meta charset="ISO-8859-1">
+<title>Thank You!</title>
+</head>
+
+<body style=>
+	<h1 style="color: #451C29; font-size:60px; text-align:center;">SPESH Market! </h1>
+	
+	<h2 style="color: #451C29; font-size:30px; text-align:center;" >Thank you for Shopping at Spesh!</h2>
+
+	<h2 style="color: #451C29; font-size:20px; text-align:center;" >You can expect your package to arrive NEVER</h2>
+    <h2 style="color: #451C29; font-size:15px; text-align:center;" >You fool. stupid idiot. why did u order things on such expensive nonsense.</h2>
+
+    <a href="Login.php"><input type="button" value="Back" style="float: left;"></a>
+		
+</body>
+</html>
