42e1ae4807b397c6e0f049180f5f161200c317ec
Sayma Haque <sayma.haque@ucalgary.ca>
2022-12-06 14:10:08 -0700

    Update ExplorePage.php

--- SPESH/Website/ExplorePage.php
+++ SPESH/Website/ExplorePage.php
@@ -30,21 +30,6 @@
     transform: translate(-50%, -50%);
     }
 
-    * {
-    box-sizing: border-box;
-    }
-
-    .row {
-    display: flex;
-    }
-
-    /* Create two equal columns that sits next to each other */
-    .column {
-    flex: 50%;
-    padding: 10px;
-    height: 300px; /* Should be removed. Only for demonstration */
-    }
-
 </style>
 </head>
   <title>Explore</title>
@@ -55,118 +40,8 @@
 		<a href="Login.php"><input type="button" value="Back" style="position:relative; font-size:12px; left: 20px; top: -170px;"></a>
         <form action="SpecificSearch.php" method="post">
 		<div>
-            <?php
-                  // Username is root
-            $user = 'root';
-            $password = '';
-            
-            $database = 'speshdb';
-            
-            // Server is localhost with
-            // port number 3306
-            $servername='localhost:3306';
-            $mysqli = new mysqli($servername, $user,
-                            $password, $database);
-            
-            // Checking for connections
-            if ($mysqli->connect_error) {
-                die('Connect Error (' .
-                $mysqli->connect_errno . ') '.
-                $mysqli->connect_error);
-            }
-            
-            $sql = " SELECT * FROM product";
-            $query = mysqli_query($mysqli, $sql);
-                        $prod = mysqli_fetch_array($query);
-            
-                        
-            $myvariable = 0;
-            $prod_id = 0;
-            $prod_name = [];
-            $prod_country = [];
-            $prod_category = [];
-            $prod_price = [];
-            $prod_img = [];
-            $prod_description = [];
-            $counter = 1;
-            while($prod_row = mysqli_fetch_array($query)){
-                $prod_id = $prod_row['id'];
-                $prod_name[$myvariable] = $prod_row['prod name'];
-                $prod_country[$myvariable] = $prod_row['country'];
-                $prod_category[$myvariable] = $prod_row['category'];
-                $prod_price[$myvariable] = $prod_row['price'];
-                $prod_img[$myvariable] = $prod_row['image url'];
-                $prod_description[$myvariable] = $prod_row['description'];
-
-                //echo $prod_country[$myvariable];
-                $myvariable = $myvariable + 1;
-            }
-
-            // $iter = 0;
-        
-            // while($iter < sizeof($prod_country)){
-            //     echo $prod_country[$iter];
-            //     echo $iter
-            //     $iter++;
-            // }
-
-            $uniqCountries = array_unique($prod_country);
-          
-            $uniqCategories = array_unique($prod_category);
-            
-            
-           
-            ?>
-
-        <div class="row">
-        <div class="column">
-            <h2>Countries:</h2>
-            <?php
-            $iter = 0;
-            while($iter < sizeof($uniqCountries)){
-            echo $uniqCountries[$iter];
-            ?>
-            <br></br>
-            <?php
-            if($iter == 3 || $iter == 13){
-                $iter = $iter + 2;
-            }
-            
-            $iter ++;
-            }
-            ?>
-    
-        </div>
-        <div class="column">
-        <h2>Categories:</h2>
-        <?php
-            $iter = 0;
-            while($iter < sizeof($uniqCategories)){
-            echo $uniqCategories[$iter];
-            ?>
-            <br></br>
-            <?php
-            if($iter == 0 ){
-                $iter = $iter + 2;
-            }
-            
-            $iter ++;
-            }
-            ?>
-            <?php
-            echo "chips"
-        ?>
-        </div>
-        </div>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
 			<label>Search By Country:</label>
 			<input type="text" name="country"/>
-            <input type="submit" value="Confirm Selection" />
             <label>OR</label>
             <br></br>
             <label>Search By Category:</label>
