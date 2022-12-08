2ce509c0a290d57daf0ffd048d9d36fc00e6d1f5
samirastk <samira.khan@ucalgary.ca>
2022-12-07 22:52:19 -0700

    sa

--- SPESH/Website/SpecificSearch.php
+++ SPESH/Website/SpecificSearch.php
@@ -63,6 +63,8 @@
                         <label>Selection 3:</label>
                         <input type="text" name="selection3"/>
                         <input type="submit" value="Confirm Selection" />
+                        <br></br>
+                        <p> Please enter painting ID into selection </p>
                     </div>
                 </form>
         <?php
