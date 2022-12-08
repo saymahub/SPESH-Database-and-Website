69f61c1aec4b524d79b6e09d8d03dc93e87e7862
samirastk <samira.khan@ucalgary.ca>
2022-12-07 20:00:48 -0700

    Update SPESHDB.sql

--- SPESH/SPESHDB.sql
+++ SPESH/SPESHDB.sql
@@ -116,28 +116,28 @@ CREATE TABLE `product` (
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`id`, `country`, `bought`, `era`, `artist`, `prod name`, `price`, `image url`) VALUES
-(0, 'United States', 'no', 'sweets', 'Ivan Aivazovsky', 'The Ninth Wave', '$101.00', "https://imgix.ranker.com/user_node_img/2231/44616753/original/the-ninth-wave-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=200"),
-(1, 'United States', 'no', 'sweets', 'Ivan Aivazovsky', 'The Ninth Wave', '$101.00', "https://imgix.ranker.com/user_node_img/2231/44616753/original/the-ninth-wave-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
-(2, 'Japan', 'no', 'sweets', 'John William Waterhouse', 'The Lady of Shalott', '$129.00', "https://imgix.ranker.com/user_node_img/475/9494074/original/the-lady-of-shalott-photo-u3?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
-(3, 'United Kingdom', 'no', 'chips', 'Pierre-Auguste Renoir', 'Dance at Le Moulin de la Galette', '$376.99', "https://imgix.ranker.com/user_node_img/27/536908/original/bal-au-moulin-de-la-galette-montmartre-photo-u2?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
-(4, 'Australia', 'no', 'chocolate', 'Hokusai', 'The Great Wave off Kanagawa', '$152.89', "https://imgix.ranker.com/user_node_img/1698/33952630/original/the-great-wave-off-kanagawa-photo-u2?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
-(5, 'United States', 'no', 'msc', 'Hieronymus Bosch', 'The Garden of Earthly Delights', '$738.89', "https://imgix.ranker.com/user_node_img/110/2194180/original/the-garden-of-earthly-delights-photo-u5?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
-(6, 'Pakistan', 'no', 'msc', 'Jean-François Millet', 'The Gleaners', '$927.99', "https://imgix.ranker.com/user_node_img/597/11936379/original/the-gleaners-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
-(7, 'Bulgaria', 'no', 'chips', 'Pierre-Auguste Renoir', 'Luncheon of the Boating Party', '$111.11', "https://imgix.ranker.com/user_node_img/75/1487553/original/luncheon-of-the-boating-party-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
-(8, 'Britain', 'no', 'chocolate', 'Georges-Pierre Seurat', 'Sunday Afternoon on the Island of La Grande Jatte', '$321.00', "https://imgix.ranker.com/user_node_img/107/2121412/original/sunday-afternoon-on-the-island-of-la-grande-jatte-photo-u3?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
-(9, 'Austrailia', 'no', 'msc', 'Gustave Caillebotte', 'Paris Street; Rainy Day', '$224.99', "https://imgix.ranker.com/user_node_img/88/1756510/original/paris-street_-rainy-day-photo-u2?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
-(10, 'Russia', 'no', 'meat', 'Édouard Manet', 'A Bar at the Folies-Bergère', '$161.00', "https://imgix.ranker.com/user_node_img/27/521266/original/a-bar-at-the-folies-berg_re-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
-(11, 'Mexico', 'no', 'msc', 'Théodore Géricault', 'The Raft of the Medusa', '$189.50', "https://imgix.ranker.com/user_node_img/94/1865958/original/the-raft-of-the-medusa-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
-(12, 'Iceland', 'no', 'meat', 'Carl Bloch', 'In a Roman Osteria', '$210.00', "https://imgix.ranker.com/user_node_img/50090/1001781107/original/in-a-roman-osteria-photo-u2?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
-(13, 'Belgium', 'no', 'sweets', 'Caspar David Friedrich', 'Moonrise Over the Sea', '$843.99', "https://imgix.ranker.com/user_node_img/567/11334768/original/moonrise-over-the-sea-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
-(14, 'Scotland', 'no', 'chips', 'Rembrandt', 'The Anatomy Lesson of Dr. Nicolaes Tulp', '$721.25', "https://imgix.ranker.com/user_node_img/23/455180/original/anatomy-lesson-of-dr-nicolaes-tulp-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
-(15, 'Japan', 'no', 'msc', 'J. M. W. Turner', 'The Fighting Temeraire', '$666.66', "https://imgix.ranker.com/user_node_img/110/2192029/original/the-fighting-temeraire-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
-(16, 'Bulgaria', 'no', 'msc', 'Emanuel Leutze', 'Washington Crossing the Delaware', '$102.50', "https://imgix.ranker.com/user_node_img/119/2362286/original/washington-crossing-the-delaware-photo-u2?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
-(17, 'Thailand', 'no', 'meat', 'Francisco Goya', 'The Third of May 1808', '$510.01', "https://imgix.ranker.com/user_node_img/112/2225478/original/the-third-of-may-1808-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
-(18, 'New Zealand', 'no', 'chips', 'Vincent van Gogh', 'The Night Café', '$402.50', "https://imgix.ranker.com/user_node_img/111/2210420/original/the-night-caf_-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
-(19, 'Japan', 'no', 'chocolate', 'Winslow Homer', 'Breezing Up', '$891.00', "https://imgix.ranker.com/user_node_img/1114/22279193/original/breezing-up-photo-u2?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
-(20, 'United States', 'no', 'msc', 'J. M. W. Turner', 'The Slave Ship', '$399.99', "https://imgix.ranker.com/user_node_img/112/2221416/original/the-slave-ship-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375");
+INSERT INTO `product` (`id`, `bought`, `era`, `artist`, `prod name`, `price`, `image url`) VALUES
+(0, 'no', 'sweets', 'Ivan Aivazovsky', 'The Ninth Wave', '$101.00', "https://imgix.ranker.com/user_node_img/2231/44616753/original/the-ninth-wave-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=200"),
+(1, 'no', 'sweets', 'Ivan Aivazovsky', 'The Ninth Wave', '$101.00', "https://imgix.ranker.com/user_node_img/2231/44616753/original/the-ninth-wave-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
+(2, 'no', 'sweets', 'John William Waterhouse', 'The Lady of Shalott', '$129.00', "https://imgix.ranker.com/user_node_img/475/9494074/original/the-lady-of-shalott-photo-u3?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
+(3, 'no', 'chips', 'Pierre-Auguste Renoir', 'Dance at Le Moulin de la Galette', '$376.99', "https://imgix.ranker.com/user_node_img/27/536908/original/bal-au-moulin-de-la-galette-montmartre-photo-u2?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
+(4, 'no', 'chocolate', 'Hokusai', 'The Great Wave off Kanagawa', '$152.89', "https://imgix.ranker.com/user_node_img/1698/33952630/original/the-great-wave-off-kanagawa-photo-u2?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
+(5, 'no', 'msc', 'Hieronymus Bosch', 'The Garden of Earthly Delights', '$738.89', "https://imgix.ranker.com/user_node_img/110/2194180/original/the-garden-of-earthly-delights-photo-u5?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
+(6, 'no', 'msc', 'Jean-François Millet', 'The Gleaners', '$927.99', "https://imgix.ranker.com/user_node_img/597/11936379/original/the-gleaners-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
+(7, 'no', 'chips', 'Pierre-Auguste Renoir', 'Luncheon of the Boating Party', '$111.11', "https://imgix.ranker.com/user_node_img/75/1487553/original/luncheon-of-the-boating-party-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
+(8, 'no', 'chocolate', 'Georges-Pierre Seurat', 'Sunday Afternoon on the Island of La Grande Jatte', '$321.00', "https://imgix.ranker.com/user_node_img/107/2121412/original/sunday-afternoon-on-the-island-of-la-grande-jatte-photo-u3?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
+(9, 'no', 'msc', 'Gustave Caillebotte', 'Paris Street; Rainy Day', '$224.99', "https://imgix.ranker.com/user_node_img/88/1756510/original/paris-street_-rainy-day-photo-u2?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
+(10, 'no', 'meat', 'Édouard Manet', 'A Bar at the Folies-Bergère', '$161.00', "https://imgix.ranker.com/user_node_img/27/521266/original/a-bar-at-the-folies-berg_re-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
+(11, 'no', 'msc', 'Théodore Géricault', 'The Raft of the Medusa', '$189.50', "https://imgix.ranker.com/user_node_img/94/1865958/original/the-raft-of-the-medusa-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
+(12, 'no', 'meat', 'Carl Bloch', 'In a Roman Osteria', '$210.00', "https://imgix.ranker.com/user_node_img/50090/1001781107/original/in-a-roman-osteria-photo-u2?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
+(13, 'no', 'sweets', 'Caspar David Friedrich', 'Moonrise Over the Sea', '$843.99', "https://imgix.ranker.com/user_node_img/567/11334768/original/moonrise-over-the-sea-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
+(14, 'no', 'chips', 'Rembrandt', 'The Anatomy Lesson of Dr. Nicolaes Tulp', '$721.25', "https://imgix.ranker.com/user_node_img/23/455180/original/anatomy-lesson-of-dr-nicolaes-tulp-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
+(15, 'no', 'msc', 'J. M. W. Turner', 'The Fighting Temeraire', '$666.66', "https://imgix.ranker.com/user_node_img/110/2192029/original/the-fighting-temeraire-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
+(16, 'no', 'msc', 'Emanuel Leutze', 'Washington Crossing the Delaware', '$102.50', "https://imgix.ranker.com/user_node_img/119/2362286/original/washington-crossing-the-delaware-photo-u2?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
+(17, 'no', 'meat', 'Francisco Goya', 'The Third of May 1808', '$510.01', "https://imgix.ranker.com/user_node_img/112/2225478/original/the-third-of-may-1808-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
+(18, 'no', 'chips', 'Vincent van Gogh', 'The Night Café', '$402.50', "https://imgix.ranker.com/user_node_img/111/2210420/original/the-night-caf_-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
+(19, 'no', 'chocolate', 'Winslow Homer', 'Breezing Up', '$891.00', "https://imgix.ranker.com/user_node_img/1114/22279193/original/breezing-up-photo-u2?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"),
+(20, 'no', 'msc', 'J. M. W. Turner', 'The Slave Ship', '$399.99', "https://imgix.ranker.com/user_node_img/112/2221416/original/the-slave-ship-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375");
 
 -- --------------------------------------------------------
 
